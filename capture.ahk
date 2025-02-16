9a11acb7175e2fec96af9a4d997aa05f76a88f67
Brad Henderson <elliotbradly@gmail.com>
2025-02-16 00:33:03 -0500

    update

--- capture.ahk
+++ capture.ahk
@@ -3,14 +3,14 @@ send {RButton}
 Send, ^a
 Sleep, 150
 Send, ^c
-Send western horror   kbar-riddlez<lora:kbar-riddlez:1.03693>
+Send fantasy supernatural   kaaarth-girl<lora:kaaarth-girl:1.403693>
 MouseMove, 900, 730
 send {RButton}
 Sleep, 150
 MouseMove, 450, 910
 send {RButton}
 Sleep, 1550
-Send image (7).png
+Send 2023-06-13-10-52-16-greenshot-gigapixel-very-compressed-scale-0-50x-7d550e51.png
 SendInput {enter}
 Sleep, 150
 MouseMove, 1633, 350
---
+++ filter.js
@@ -0,0 +1,59 @@
+var FS = require('fs-extra')
+var S = require('string')
+var Chance = require('chance');
+var chance = new Chance();
+
+var list = FS.readdirSync('/./filter')
+
+
+list.forEach((a, b) => {
+
+    var png = false;
+    var jpg = false;
+
+    var hmm = a.split('.')
+    var itm = hmm.pop()
+
+    if (itm == null) return
+
+    if (itm == 'png') png = true
+    if (itm == 'jpg') jpg = true
+    if (itm == 'jpeg') jpg = true
+    if (itm == 'JPG') jpg = true
+
+
+    if (png == false && jpg == false) return console.log('not present ' + a)
+
+    a = a.toLowerCase()
+
+    var now = S(a).replaceAll('.png', '')
+    now = S(now).replaceAll('.jpg', '')
+    now = S(now).slugify().s
+
+    var pin = chance.bb_pin()
+
+    var file = now + '-' + pin;
+
+    png
+    jpg
+
+    if (png == true) { file += '.png' }
+    if (jpg == true) { file += '.jpg' }
+
+    var input = '../filter/' + a
+    var output = "../image/" + file
+
+    FS.copyFileSync( input, output)
+    FS.removeSync( input )
+
+    console.log('check... ' + output)
+
+    //check to see if png
+    //check to see if jpg
+    //return if not an image 
+
+
+
+
+
+})
